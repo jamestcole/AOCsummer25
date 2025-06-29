adcc73caf2a326dfe37923a139e2f9ab4d6513ef
jamestcole <jamestcole@yahoo.co.uk>
2025-06-29 17:12:54 +0100

    pt2day9

--- day9solutionpt1.py
+++ day9solutionpt1.py
@@ -1,15 +1,15 @@
 import timeit
 
-#grid = [
-#"2199943210",
-#"3987894921",
-#"9856789892",
-#"8767896789",
-#"9899965678"
-#]
+grid = [
+"2199943210",
+"3987894921",
+"9856789892",
+"8767896789",
+"9899965678"
+]
 
-grid = open("2021Day9.txt","r")
-grid = [line.strip() for line in grid.readlines()]
+#grid = open("2021Day9.txt","r")
+#grid = [line.strip() for line in grid.readlines()]
 
 def find_low_points():
     low_points = []
@@ -24,6 +24,7 @@ def find_low_points():
             # Check all adjacent positions
             for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 ni, nj = i + di, j + dj
+                (ni,nj)
                 if 0 <= ni < rows and 0 <= nj < cols:
                     if int(grid[ni][nj]) <= current:
                         is_low_point = False
